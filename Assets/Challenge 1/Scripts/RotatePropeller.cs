dce1277e6ac77fe9115261d580beae50ee510109
josepjesus <pepjesus@hotmail.es>
2020-04-05 23:41:26 +0200

    Unit 1 de Create With Code terminado

---
+++ Assets/Challenge 1/Scripts/RotatePropeller.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatePropeller : MonoBehaviour
+{
+    private float propSpeed = 1000;
+    void Update()
+    {
+        transform.Rotate(Vector3.forward, propSpeed * Time.deltaTime);
+    }
+
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    //public float speed = 5.0f;
+    //public float turnspeed = 25.0f;
+    //public float horizontalinput;
+    //public float forwardInput;
+
+    private float speed = 20.0f;
+    private float turnSpeed = 45.0f;
+    private float horizontalInput;
+    private float forwardInput;
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+        //Move the Vehicle forward
+        //transform.Translate(0,0,1);
+        //transform.Translate(Vector3.forward * Time.deltaTime * 20);
+        //transform.Translate(Vector3.forward * Time.deltaTime * speed);
+
+        //Moves the Vehicle forward or backward based on vertical input
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+
+        //transform.Translate(Vector3.right * Time.deltaTime * turnSpeed);
+        //transform.Translate(Vector3.right * Time.deltaTime * horizontalInput);
+        //transform.Rotate(Vector3.up, horizontalInput);
+
+        //Rotates the Vehicle based on horizontal input
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+    }
+
+
+}
